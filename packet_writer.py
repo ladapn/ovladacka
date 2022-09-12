b93c6eac18fe83b3e178b933e127b7f8107631de
Ladapn <2048042+ladapn@users.noreply.github.com>
2022-09-12 16:05:37 +0200

    Write robot outputs to dedicated folder
    To improve output folder structure, all outputs are now stored in
    "robot_outputs" parent folder and each run is further stored in
    subfolder containing date and time of its creation in its name.

--- packet_writer.py
+++ packet_writer.py
@@ -1,5 +1,6 @@
 import time
 import csv
+import pathlib
 import pandas as pd
 from abc import ABC, abstractmethod
 
@@ -7,6 +8,8 @@ from abc import ABC, abstractmethod
 class PacketWriter(ABC):
     def __init__(self):
         self.time_string = time.strftime("%Y-%m-%d-%H-%M-%S")
+        self.path = pathlib.Path('robot_outputs/' + self.time_string)
+        self.path.mkdir(exist_ok=True)
 
     @abstractmethod
     def write_packet(self, packet_id, packet_data):
@@ -20,7 +23,7 @@ class PacketWriter(ABC):
 class USNDPacketWriter(PacketWriter):
     def __init__(self, usnd_packet_ids):
         super().__init__()
-        self.usnd_file = open(self.time_string + '.csv', 'w')
+        self.usnd_file = open(self.path / (self.time_string + '.csv'), 'w')
         self.usnd_writer = csv.writer(self.usnd_file)
         self.usnd_writer.writerow(['id', 'tick_ms', 'distance_cm', 'crc'])
         self.data_frame_dict = {}
@@ -46,13 +49,13 @@ class USNDPacketWriter(PacketWriter):
         # data_frame['Min'] = data_frame[['101', '102', '103']].min(axis=1)
         data_frame.columns = ['front', 'right_front', 'right_center', 'right_back']
         data_frame['Min'] = data_frame.min(axis=1)
-        data_frame.to_csv(time.strftime("%Y-%m-%d-%H-%M-%S") + '_pd.csv')
+        data_frame.to_csv(self.path / (self.time_string + '_pd.csv'))
 
 
 class StatusPacketWriter(PacketWriter):
     def __init__(self):
         super().__init__()
-        self.status_file = open(self.time_string + '_stat.csv', 'w')
+        self.status_file = open(self.path / (self.time_string + '_stat.csv'), 'w')
         self.status_csv = csv.writer(self.status_file)
         self.status_csv.writerow(
             ['id', 'tick_ms', 'commit_id', 'battery_v_adc', 'total_i_adc', 'motor_i_adc', 'crc'])
