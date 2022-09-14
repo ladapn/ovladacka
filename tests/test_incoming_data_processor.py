4438347213b0622bc4ff2704cf28c797da0741c8
Ladapn <2048042+ladapn@users.noreply.github.com>
2022-09-14 18:31:02 +0200

    Improve test coverage for InputDataProcessor

--- tests/test_incoming_data_processor.py
+++ tests/test_incoming_data_processor.py
@@ -1,4 +1,6 @@
+import io
 import unittest
+import unittest.mock
 import incoming_data_processor
 
 
@@ -6,13 +8,35 @@ class InputDataProcessorTestCase(unittest.TestCase):
     def test_process_incoming_data(self):
         input_data_processor = incoming_data_processor.InputDataProcessor()
 
-        processed_data = input_data_processor.\
-            process_incoming_data(data=b'P\x1f\x9d\x06\x00T\xd6\xa7\nu\x00\x00\x00\x00\x00\x8ed\xc3\x89\x00\x00u\x00\x00\x00[')
+        processed_data = input_data_processor.process_incoming_data(data=b'P\x1f\x9d\x06\x00T\xd6\xa7\nu\x00\x00\x00'
+                                                                         b'\x00\x00\x8ed\xc3\x89\x00\x00u\x00\x00\x00[')
 
         self.assertCountEqual(processed_data,
                               [(80, [80, 433439, 178771540, 117, 0, 0, 142]),
                                (100, [100, 35267, 117, 91])]
                               )
+
+    def test_process_incoming_data_two_bursts(self):
+        input_data_processor = incoming_data_processor.InputDataProcessor()
+
+        processed_data = input_data_processor.\
+            process_incoming_data(data=b'P\x1f\x9d\x06\x00T\xd6\xa7\nu\x00\x00\x00\x00\x00\x8ed\xc3\x89')
+
+        processed_data += input_data_processor.\
+            process_incoming_data(data=b'\x00\x00u\x00\x00\x00[')
+
+        self.assertCountEqual(processed_data,
+                              [(80, [80, 433439, 178771540, 117, 0, 0, 142]),
+                               (100, [100, 35267, 117, 91])]
+                              )
+
+    @unittest.mock.patch('sys.stdout', new_callable=io.StringIO)
+    def test_process_incoming_bad_checksum(self, mock_stdout):
+        input_data_processor = incoming_data_processor.InputDataProcessor()
+        processed_data = input_data_processor. \
+            process_incoming_data(data=b'd\xc3\x89\x00\x00u\x00\x00\x006')
+        self.assertTrue('Broken checksum found, Packet ID: 100' in mock_stdout.getvalue())
+
 
 
 if __name__ == '__main__':
