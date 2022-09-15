263ab2360d219d104be48b5195d63c724064520a
Ladapn <2048042+ladapn@users.noreply.github.com>
2022-09-15 18:37:05 +0200

    Share USNDPAcketWriter instance among usnd packets
    By mistake, each ultrasound packet got its own instance of writer, which
    makes no sense, because all these measurements should be put together.

--- input_data_writer.py
+++ input_data_writer.py
@@ -5,14 +5,17 @@ class InputDataWriter:
     def __init__(self, id_to_writer_type): # plus header...
         self.id_to_writer = {}
 
-        # fixme
+        # fixme -> shouldn't be nescessary...
         usnd_packet_ids = [100, 101, 102, 103]
+        usnd_writer = packet_writer.USNDPacketWriter(usnd_packet_ids)
 
         for packet_id in id_to_writer_type:
             if id_to_writer_type[packet_id]['writer'] == 'generic':
-                self.id_to_writer[packet_id] = packet_writer.StatusPacketWriter() # FIXME separate instances?
+                self.id_to_writer[packet_id] = packet_writer.StatusPacketWriter()
             elif id_to_writer_type[packet_id]['writer'] == 'ultrasound':
-                self.id_to_writer[packet_id] = packet_writer.USNDPacketWriter(usnd_packet_ids)
+                self.id_to_writer[packet_id] = usnd_writer
+
+        # FIXME vytvor primo instanci podle jmena
 
     def write(self, packets):
         for packet_id, packet_data in packets:
