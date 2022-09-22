85a774d770540d7f866db959cfde9a443d796b1c
Ladapn <2048042+ladapn@users.noreply.github.com>
2022-09-22 21:40:12 +0200

    Add docstrings to rest of connection module

--- connection/robot_connection.py
+++ connection/robot_connection.py
@@ -2,26 +2,53 @@ from abc import ABC, abstractmethod
 
 
 class RobotConnection(ABC):
+    """
+    Abstract class defining interface for connection to robot.
+    """
     @abstractmethod
     def connect(self, number_of_retries=3):
+        """
+        Initiate connection to robot
+        :param int number_of_retries: Total number of attempts to connect, if connection cannot be established,
+        defaults to 3
+        """
         pass
 
     @abstractmethod
     def wait_for_notifications(self, timeout):
+        """
+        Wait for notification that there are new data from robot available. The method blocks until
+        new data is available, or until maximal waiting time elapses
+        :param float timeout: maximal waiting time in seconds
+        :return True if data received, False in case of timeout
+        """
         pass
 
     @abstractmethod
     def write(self, data):
+        """
+        Send data to robot
+        :param bytes data: data to be sent
+        """
         pass
 
     @abstractmethod
     def disconnect(self):
+        """
+        Disconnects from robot
+        """
         pass
 
     def __enter__(self):
+        """
+        If context manager is entered, initiate robot connection
+        """
         self.connect()
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
+        """
+        If context manager is exited, disconnect from robot
+        """
         self.disconnect()
 
--- connection/simulated_connection.py
+++ connection/simulated_connection.py
@@ -2,19 +2,45 @@ from connection.robot_connection import RobotConnection
 
 
 class SimConnection(RobotConnection):
+    """
+    Implements a simulated connection to robot. This is intended to run ovladacka without robot mainly for testing and
+    development purposes
+    """
     def __init__(self, data_queue):
+        """
+        Constructor method
+        :param queue.Queue data_queue: queue to hold data received from simulated robot
+        """
         self.data_queue = data_queue
 
     def connect(self, number_of_retries=3):
+        """
+        In simulation mode, we do not connect to any device so this implementation of RobotConnection's connect does
+        nothing
+        """
         pass
 
     def wait_for_notifications(self, timeout):
+        """
+        Puts fake data in the data queue to simulate that robot sent something. Currently, one packet 80 with fixed data
+        is used - i.e. everytime this method is called, the same data is put to the queue
+        :param timeout:
+        :return:
+        """
         data = b'P\x1f\x9d\x06\x00T\xd6\xa7\nu\x00\x00\x00\x00\x00\x8e'
         self.data_queue.put(data)
 
     def write(self, data):
+        """
+        Currently, the simulation does not implement sending data to robot so this implementation of RobotConnection's
+        write does nothing
+        """
         pass
 
     def disconnect(self):
+        """
+        In simulation mode, we do not connect to any device so this implementation of RobotConnection's disconnect does
+        nothing
+        """
         pass
 
