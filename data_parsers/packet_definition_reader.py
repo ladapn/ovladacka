36cca6338e11e0fdf85af3d201759bda440871e1
Ladapn <2048042+ladapn@users.noreply.github.com>
2022-09-29 14:14:59 +0200

    Add docstring to read_packet_definition

--- data_parsers/packet_definition_reader.py
+++ data_parsers/packet_definition_reader.py
@@ -1,8 +1,13 @@
 import json
 
 
-def read_packet_definition(definition_path):
-    with open(definition_path, 'r') as definition_file:
+def read_packet_definition(definition_file_name):
+    """
+    Read from a json file definition of packets coming from robot
+    :param definition_file_name: filename of the configuration file
+    :return: dictionary with packet id as a key - this dictionary holds another dictionary containing packet definitions
+    """
+    with open(definition_file_name, 'r') as definition_file:
         definitions = json.load(definition_file)
 
     # TODO: perform input validity checks
