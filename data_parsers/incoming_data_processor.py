d7aec7e1476085a35156b9793927054a19c98d29
Ladapn <2048042+ladapn@users.noreply.github.com>
2022-09-29 20:35:28 +0200

    Add docstring to InputDataProcessor

--- data_parsers/incoming_data_processor.py
+++ data_parsers/incoming_data_processor.py
@@ -2,11 +2,28 @@ from data_parsers import packet_parser
 
 
 class InputDataProcessor:
+    """
+    Class to process data coming from robot
+    """
     def __init__(self, packet_definition):
+        """
+        Constructor method
+        :param packet_definition: dictionary with packet ID (int) as a key - the value is another dictionary, with
+        packet field name in string form as a key and its value. At least 'length' and 'structure' fields
+        must be provided, where 'length' represents overall packet length in bytes and 'structure' is a format string
+        used by struct package (see https://docs.python.org/3/library/struct.html) to decode packet field from binary
+        data
+        """
         self.leftovers = None
         self.packet_definition = packet_definition
 
     def process_incoming_data(self, data):
+        """
+        Process data coming from robot
+        :param bytes data: raw data sent by robot
+        :return: list of tuples, where each item in the list corresponds to one parsed packet, first element of the
+        tuple is packet ID, the other element is a list of parsed packet fields
+        """
         print("data received")
         print(data)
 
