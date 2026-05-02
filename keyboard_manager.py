71d14d55c2e66d7c939e8fddce2606b1903281ab
Ladapn <2048042+ladapn@users.noreply.github.com>
2026-05-02 18:21:05 +0200

    Migrate from pynput to PyQt

--- keyboard_manager.py
+++ keyboard_manager.py
@@ -1,48 +1,87 @@
-from pynput.keyboard import Key, Listener
 import queue
+import sys
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+KEY_TO_ROBOT_COMMAND = {
+    QtCore.Qt.Key_Up: b':a!',
+    QtCore.Qt.Key_Down: b':c!',
+    QtCore.Qt.Key_Right: b':b!',
+    QtCore.Qt.Key_Left: b':d!',
+    QtCore.Qt.Key_Space: b':e!',
+    QtCore.Qt.Key_Shift: b':f!'
+}
+TERMINATION_KEY = QtCore.Qt.Key_Escape
 
 
 def key_to_robot_command(key):
-    return {
-        Key.up: b'a!',
-        Key.down: b'c!',
-        Key.right: b'b!',
-        Key.left: b'd!',
-        Key.space: b'e!',
-        Key.shift: b'f!'
-    }.get(key, None)
+    return KEY_TO_ROBOT_COMMAND.get(key, None)
 
 
 class TerminationRequested(Exception):
     pass
 
 
+class KeyboardWidget(QtWidgets.QWidget):
+    key_pressed = QtCore.pyqtSignal(int)
+
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle('Ovladacka Keyboard Control')
+        self.setFixedSize(340, 100)
+        self.setFocusPolicy(QtCore.Qt.StrongFocus)
+
+        label = QtWidgets.QLabel(
+            'Focus this window and press Arrow keys, Space, Shift or Escape to exit.',
+            self
+        )
+        label.setWordWrap(True)
+
+        layout = QtWidgets.QVBoxLayout(self)
+        layout.addWidget(label)
+
+    def keyPressEvent(self, event):
+        key = event.key()
+        self.key_pressed.emit(key)
+        event.accept()
+
+
 class KeyboardManager:
     def __init__(self):
         self.key_queue = queue.Queue()
-        self.listener = Listener(on_press=self.on_press)
+
+        self.app = QtWidgets.QApplication.instance()
+        self._owns_app = False
+        if self.app is None:
+            self.app = QtWidgets.QApplication(sys.argv)
+            self._owns_app = True
+
+        self.widget = KeyboardWidget()
+        self.widget.key_pressed.connect(self.on_press)
 
     def on_press(self, key):
-        """Callback to handle key press event"""
-        print(f'{key} pressed')
-
+        key_text = QtGui.QKeySequence(key).toString() or str(key)
+        print(f'{key_text} pressed')
         self.key_queue.put(key)
 
-        if key == Key.esc:
-            return False
-        else:
-            return True
-
     def start(self):
-        self.listener.start()
+        self.widget.show()
+        self.widget.activateWindow()
+        self.widget.raise_()
+        self.widget.setFocus()
 
     def stop(self):
-        self.listener.stop()
+        self.widget.close()
+        if self._owns_app:
+            self.app.quit()
 
     def get_key_nowait(self):
+        self.app.processEvents(QtCore.QEventLoop.AllEvents)
+
         try:
             key = self.key_queue.get_nowait()
-            if key == Key.esc:
+            if key == TERMINATION_KEY:
                 raise TerminationRequested
         except queue.Empty:
             key = None
